3967c6e26969d978f9fbabb34b69a8a1ce6c299c
swapnareddyanumula <swapnareddyanumula@gmail.com>
2021-11-18 20:44:51 +0530

    second commit

---
+++ practice.js
@@ -0,0 +1,18 @@
+let s = "The min and max joining date from all the student's data";
+let m = s.match("joining");
+if (m=="joining")
+{
+    console.log("joining"); 
+}
+else
+{
+    console.log("search word not found");
+}
+
+let f = "global.mp4";
+let sp = f.split(".");
+console.log(sp[1]);
+
+
+
+
