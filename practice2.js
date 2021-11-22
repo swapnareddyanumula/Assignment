78f1ca5f0090370edcc0817fae1a89c278c5c05b
swapnareddyanumula <swapnareddyanumula@gmail.com>
2021-11-22 19:05:07 +0530

    third commit

---
+++ practice2.js
@@ -0,0 +1,74 @@
+let count = 5;
+console.log(count);
+
+let myage = 28;
+console.log(myage)
+
+let firstbatch = 10
+let secondbatch = 6
+count = firstbatch + secondbatch
+console.log(count)
+
+let humandogratio = 7
+let mydogage = myage * humandogratio
+console.log(mydogage)
+
+let bonuspoints = 50
+console.log(bonuspoints)
+
+bonuspoints = bonuspoints + 50
+console.log(bonuspoints)
+
+bonuspoints = bonuspoints - 75
+console.log(bonuspoints)
+
+bonuspoints = bonuspoints + 45
+console.log(bonuspoints)
+
+function func()
+{
+    console.log("Function called")
+}
+func()
+
+let a = 10
+let b= 40
+let c= 55
+function func1()
+{
+    console.log(a+b+c)
+}
+func1()
+
+let x=0
+function func2()
+{
+    x= x+1
+}
+func2()
+func2()
+func2()
+console.log(x)
+
+let message= "You have three new notifications"
+let username = "Swapna"
+console.log(message)
+//console.log(message +" "+ username)
+let messagetouser = message +" "+ username
+console.log(messagetouser)
+
+let names = "Teena"
+let greeting = "Hi, my name is "
+let mygreeting = greeting + names
+console.log(mygreeting)
+
+let counts = 40
+let bonuscounts = "20"
+let totalcounts = counts+bonuscounts
+console.log(totalcounts)
+    
+console.log(2+3)
+console.log("2"+"3")
+console.log("2"+3)
+console.log(2+"3")
+
